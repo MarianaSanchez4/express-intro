150221303f2455ee9f5b792f5a4ffecff0b12375
Eric Michael Martinez <eric.michael.mtz@gmail.com>
2020-02-10 15:02:09 -0600

    example

--- index.js
+++ index.js
@@ -13,4 +13,19 @@ var server = app.listen(app.get('port'), function() {
 
 app.get("/hello", function(req, res){
     res.send("Hello World Directly From Express");
-})
+});
+
+app.get("/students.json", function(req, res){
+    var data = [{"id":1,"first_name":"Emmy","last_name":"Gradley","grade":29},
+    {"id":2,"first_name":"Christean","last_name":"Deeny","grade":41},
+    {"id":3,"first_name":"Maxie","last_name":"Goldthorpe","grade":86},
+    {"id":4,"first_name":"Free","last_name":"Novkovic","grade":33},
+    {"id":5,"first_name":"Alexandros","last_name":"Paliser","grade":32},
+    {"id":6,"first_name":"Vannie","last_name":"Bowler","grade":31},
+    {"id":7,"first_name":"Cy","last_name":"Cheshire","grade":88},
+    {"id":8,"first_name":"Gray","last_name":"Eardley","grade":57},
+    {"id":9,"first_name":"Kessiah","last_name":"Breedy","grade":16},
+    {"id":10,"first_name":"Wilden","last_name":"Chastaing","grade":65}];
+    
+    res.json(data);
+});
