4a3fd6ac02c63f741e7723a099f11cf265a58277
Eric Michael Martinez <eric.michael.mtz@gmail.com>
2020-02-10 14:28:02 -0600

    add scaffolding

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var path = require('path')
+
+app = express()
+
+app.set('port', 3002)
+
+app.use(express.static(path.join(__dirname, 'static')))
+
+var server = app.listen(app.get('port'), function() {
+	console.log("Server started...")
+})
+
+app.get("/hello", function(req, res){
+    res.send("Hello World Directly From Express");
+})
